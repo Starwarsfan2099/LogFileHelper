3b0e26a024db3c77b37781b03576f57b5af6cd5f
Starwarsfan2099 <starwarsfan2099@gmail.com>
2019-11-11 13:45:06 -0500

    Update

--- test_NCL_error_log.py
+++ test_NCL_error_log.py
@@ -7,22 +7,22 @@ logfile = LogFileHelper.ParseLogFile(fileName, verbose=verbose)
 
 # Question 1
 errors = logfile.countOccurrencesByLine("File does not exist")
-print "%s \'File does not exist\' errors." % errors
+print "1: %s \'File does not exist\' errors." % errors
 
 # Question 2
 count = len(logfile.getColumnUniqueValuesSorted(7))
-print "Unique IP addresses: %s" % count
+print "2: Unique IP addresses: %s" % count
 
 # Question 3
 ip = logfile.getColumnUniqueValuesCounted(5, "[error]", 7, largestFirst=True)
 mostErrors = logfile.getLargestItemInDictionary(ip)
-print "IP with most errors: %s" % mostErrors.strip("]")
+print "3: IP with most errors: %s" % mostErrors.strip("]")
 
 # Question 4
 stuff = logfile.getColumnUniqueValuesByColumn(7, -1)
 mostUniqueFiles = logfile.getMostItemsForKeyInDictionary(stuff).strip("]")
-print "IP with most unique requested files: %s" % mostUniqueFiles
+print "4: IP with most unique requested files: %s" % mostUniqueFiles
 
 # Question 5
 count = logfile.countUniqueOccurrencesByLineIfColumn("File does not exist", 7, mostUniqueFiles, 12)
-print "Unique file/directory requests from %s that yielded a \'Does not exists\': %s" % (mostUniqueFiles, count)
+print "5: Unique file/directory requests from %s that yielded a \'Does not exists\': %s" % (mostUniqueFiles, count)
